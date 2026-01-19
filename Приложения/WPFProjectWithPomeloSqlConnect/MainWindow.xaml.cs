8da63ea3217e2d5ccff79f65edf608ce411c7da3
YaeKitsune <aepnaonp@gmail.com>
2026-01-20 04:03:59 +0500

    Add Pomelo Test App

---
+++ Приложения/WPFProjectWithPomeloSqlConnect/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFProjectWithPomeloSqlConnect.Models;
+
+namespace WPFProjectWithPomeloSqlConnect;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public AppDbContext appDb;
+    public MainWindow()
+    {
+        InitializeComponent();
+        appDb = new AppDbContext();
+        UpdateUsersDataGrid();
+    }
+
+    public void UpdateUsersDataGrid()
+    {
+        var users = appDb.Users.ToList();
+        UsersDataGrid.ItemsSource = users;
+        MessageBox.Show(users.Count.ToString());
+    }
+}
---
+++ Приложения/WPFProjectWithPomeloSqlConnect/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPFProjectWithPomeloSqlConnect.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+}
