efc6fb1d3bfcff91cb4bcf2d6596eb32fbfd6903
YaeKitsune <aepnaonp@gmail.com>
2026-02-25 13:02:56 +0500

    completed database including to c# project and go starting work procedures and functions

---
+++ Приложениие/ImportAndVisualiseTwoTables/ImportAndVisualiseTwoTables/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ImportAndVisualiseTwoTables.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImportAndVisualiseTwoTables;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly InsuranceContext dbContext;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        dbContext = new InsuranceContext();
+        LoadEmployeesDataGrid();
+        LoadClaimsDataGrid();
+    }
+
+    private void LoadEmployeesDataGrid()
+    {
+        var dbEmployees = dbContext.Employees.ToList();
+        EmployeesDataGrid.ItemsSource = dbEmployees;
+    }
+
+    private void LoadClaimsDataGrid()
+    {
+        ClaimsIncludedDataGrid.ItemsSource = dbContext.Claims
+            .Include(c => c.PolicyNumberNavigation)
+            .AsNoTracking()
+            .ToList();
+    }
+}
---
+++ Приложениие/ImportAndVisualiseTwoTables/ImportAndVisualiseTwoTables/Models/InsuranceTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImportAndVisualiseTwoTables.Models;
+
+public partial class InsuranceTypes
+{
+    public short InsuranceTypeId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? AnnualCost { get; set; }
+
+    public virtual ICollection<Policyholders> Policyholders { get; set; } = new List<Policyholders>();
+}
