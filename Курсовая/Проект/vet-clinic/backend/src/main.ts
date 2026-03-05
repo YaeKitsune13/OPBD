b9eb7c13c9f334e5d27ac00c86d3946a6e0c5899
YaeKitsune <aepnaonp@gmail.com>
2026-03-05 12:30:00 +0500

    add bat from start server, add README and swagger

--- Курсовая/Проект/vet-clinic/backend/src/main.ts
+++ Курсовая/Проект/vet-clinic/backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle("API Ветеренарной клиники")
+    .setDescription("API для учёта посещений ветеренарной клиники")
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
