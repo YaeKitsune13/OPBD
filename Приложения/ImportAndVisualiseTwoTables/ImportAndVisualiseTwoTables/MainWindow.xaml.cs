c58438062a2ee685b045b8cd5ce04e7662ab4270
YaeKitsune <aepnaonp@gmail.com>
2026-03-11 13:18:24 +0500

    remove data

--- Приложения/ImportAndVisualiseTwoTables/ImportAndVisualiseTwoTables/MainWindow.xaml.cs
+++ Приложения/ImportAndVisualiseTwoTables/ImportAndVisualiseTwoTables/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows.Controls;
 using ImportAndVisualiseTwoTables.Models;
 using Microsoft.EntityFrameworkCore;
+using ImportAndVisualiseTwoTables.ModelsPostgresSQL;
 
 namespace ImportAndVisualiseTwoTables;
 
@@ -10,18 +11,20 @@ namespace ImportAndVisualiseTwoTables;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private readonly InsuranceContext dbContext;
+    private readonly Models.InsuranceContext dbContext;
     private readonly ModelsPostgresSQL.InsuranceContext dbContext2;
     private List<int> employeeIds = new List<int>();
     public MainWindow()
     {
         InitializeComponent();
-        dbContext = new InsuranceContext();
+        dbContext = new Models.InsuranceContext();
         dbContext2 = new ModelsPostgresSQL.InsuranceContext();
         LoadEmployeesDataGrid();
         LoadClaimsDataGrid();
-        LoadEmployersComboBox();
-        LoadPolicyHoldersDataGrid();
+        // LoadEmployersComboBox();
+        // LoadPolicyHoldersDataGrid();
+        // LoadInsuranceTypes();
+        // LoadInsuranceClaimsByType();
     }
 
     private void LoadEmployeesDataGrid()
@@ -29,8 +32,21 @@ public partial class MainWindow : Window
         var dbEmployees = dbContext.Employees
             .FromSqlRaw("SELECT * FROM employees")
             .ToList();
-    
+
         EmployeesDataGrid.ItemsSource = dbEmployees;
+        EmployeesDataGrid.SelectionChanged += EmployeesDataGrid_SelectionChanged;
+    }
+
+    private void EmployeesDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (EmployeesDataGrid.SelectedItem is Models.Employees selectedEmployee)
+        {
+            ClaimsIncludedDataGrid.ItemsSource = dbContext.Claims
+                .Include(c => c.PolicyNumberNavigation)
+                .Where(c => c.PolicyNumberNavigation!.EmployeeId == selectedEmployee.EmployeeId)
+                .AsNoTracking()
+                .ToList();
+        }
     }
 
     private void LoadClaimsDataGrid()
@@ -41,33 +57,5 @@ public partial class MainWindow : Window
             .ToList();
     }
 
-    private void LoadEmployersComboBox()
-    {
-        employeeIds = dbContext.Employees.Select(e => e.EmployeeId).ToList();
-        DatabaseComboBox.ItemsSource = employeeIds;
-
-        DatabaseComboBox.SelectionChanged += (s, e) =>
-        {
-            if (DatabaseComboBox.SelectedItem is int selectedEmployeeId)
-            {
-                PolicyHoldersDataGrid.ItemsSource = dbContext2.Policyholders
-                    .Where(p => p.EmployeeId == selectedEmployeeId)
-                    .AsNoTracking()
-                    .ToList();
-            }
-        };
-    }
-
-    private void LoadPolicyHoldersDataGrid()
-    {
-        PolicyHoldersDataGrid.ItemsSource = dbContext2.Policyholders
-            .AsNoTracking()
-            .ToList();
-    }
-
-    private void LoadButton_Click(object sender, RoutedEventArgs e)
-    {
-        LoadPolicyHoldersDataGrid();
-    }
-
+   
 }
