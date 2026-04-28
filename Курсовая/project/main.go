7581f67710d2c72e74ec7d048a28fcf9c36d6fd5
YaeKitsune <aepnaonp@gmail.com>
2026-04-28 15:46:39 +0500

    golang initialize and base test

--- Курсовая/project/main.go
+++ Курсовая/project/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type data struct {
+	ID int8 `json:"id"` // Тег пишется в обратных кавычках
+}
+
 func main() {
 	fmt.Println("Hello, World!")
+
+	// Используем := для объявления и соблюдаем регистр поля ID
+	user := data{ID: 3}
+
+	fmt.Printf("User ID: %d\n", user.ID)
+	printMessage() // Переименовано, чтобы не путать со встроенными именами
+}
+
+func printMessage() {
+	fmt.Println("HElfods")
 }
