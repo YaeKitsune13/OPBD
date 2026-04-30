c477b7feef8ab6fe1aaf3c050aa9db642d617622
YaeKitsune <aepnaonp@gmail.com>
2026-04-30 23:32:53 +0500

    added pets model

---
+++ Курсовая/project/models/pets.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Pet struct {
+	PetID         int64     `json:"pet_id" gorm:"primaryKey;autoIncrement"`
+	OwnerID       int64     `json:"owner_id" gorm:"not null"`
+	Owner         Owner     `json:"-" gorm:"foreignKey:OwnerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Nickname      string    `json:"nickname" orm:"type:varchar(30);not null"`
+	Species       string    `json:"species" gorm:"type:varchar(50);not null"`
+	Breed         string    `json:"breed" gorm:"type:varchar(100)"`
+	BirthDate     time.Time `json:"birth_date" gorm:"type:date;not null"`
+	Photo         string    `json:"photo" gorm:"type:varchar(255)"`
+	CurrentWeight float64   `json:"current_weight" gorm:"type:decimal(5,2);check:current_weight > 0.1 AND current_weight < 300"`
+}
