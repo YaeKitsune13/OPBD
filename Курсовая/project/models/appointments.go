a05e2856ec646d52c4dd15eaff4c7137a5469831
YaeKitsune <aepnaonp@gmail.com>
2026-04-30 23:38:40 +0500

    update appoinments

--- Курсовая/project/models/appointments.go
+++ Курсовая/project/models/appointments.go
@@ -2,12 +2,22 @@ package models
 
 import "time"
 
-type Appointments struct {
+type Status string
+
+const (
+	StatusWaiting   Status = "waiting"
+	StatusConfirmed Status = "confirmed"
+	StatusRejected  Status = "rejected"
+)
+
+type Appointment struct {
 	AppointmentId int64     `json:"appointment_id" gorm:"primaryKey;autoIncrement"`
 	PetId         int64     `json:"pet_id" gorm:"not null"`
 	Pet           Pet       `json:"-" gorm:"foreignKey:PetId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	DoctorId      int64     `json:"doctor_id" gorm:"not null"`
-	Doctor        Doctor    `json:"-" gorm:"foreignKey:DoctorID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	ScheduledAt   time.Time `json:"scheduled_at" gorm:"type:datetime;not null"`
-	Comment       string    `json:"comment"`
+	Doctor        Doctor    `json:"-" gorm:"foreignKey:DoctorId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ScheduledAt   time.Time `json:"scheduled_at" gorm:"type:timestamp;not null"`
+	Comment       string    `json:"comment" gorm:"type:text"`
+	Status        Status    `json:"status" gorm:"type:varchar(20);default:waiting;check:status IN ('waiting','confirmed','rejected')"`
+	CreatedAt     time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
