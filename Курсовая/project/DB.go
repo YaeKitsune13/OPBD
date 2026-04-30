f764614d607002c4c50cccd2c33eeda64270fb83
YaeKitsune <aepnaonp@gmail.com>
2026-05-01 01:04:59 +0500

    Updated db.go from auto migrate

--- Курсовая/project/DB.go
+++ Курсовая/project/DB.go
@@ -1,17 +1,12 @@
 package main
 
 import (
+	"example/project/models"
 	"fmt"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
-
-// Модель вашей таблицы
-type User struct {
-	gorm.Model
-	Name string
-}
 
 func ConnectDB() (*gorm.DB, error) {
 	dsn := "host=localhost user=postgres password=1234 dbname=cursavoi_project port=5432 sslmode=disable"
@@ -20,7 +15,17 @@ func ConnectDB() (*gorm.DB, error) {
 		return nil, err
 	}
 
-	err = db.AutoMigrate()
+	err = db.AutoMigrate(
+		&models.Appointment{},       // 1
+		&models.Doctor{},            // 2
+		&models.Medication{},        // 3
+		&models.Owner{},             // 4
+		&models.Pet{},               // 5
+		&models.Service{},           // 6
+		&models.Visit{},             // 7
+		&models.VisitPrescription{}, // 8
+		&models.WeightHistory{},     // 9
+	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to migrate: %v", err)
 	}
