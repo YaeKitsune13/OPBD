84b904a94e916ee109accb070652658fdfc2ffa3
YaeKitsune <aepnaonp@gmail.com>
2026-05-05 10:10:04 +0500

    add revenue reportdto dto

---
+++ Курсовая/project/backend/dto/RevenueReportDTO.go
@@ -0,0 +1,15 @@
+package dto
+
+type DailyRows struct {
+	Date     string `json:"date"`
+	Visits   string `json:"visits"`
+	Services string `json:"services"`
+	Meds     string `json:"meds"`
+	Total    string `json:"total"`
+}
+
+type RevenueReportDTO struct {
+	PeriodTotal   int64     `json:"periodTotal"`
+	ServicesTotal int64     `json:"servicesTotal"`
+	DailyRows     DailyRows `json:"dailyRows"`
+}
