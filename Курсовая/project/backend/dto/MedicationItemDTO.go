ef7e530f33617c05dc573e00628296569b52e8e0
YaeKitsune <aepnaonp@gmail.com>
2026-05-05 10:09:36 +0500

    add medicationitem dto

---
+++ Курсовая/project/backend/dto/MedicationItemDTO.go
@@ -0,0 +1,17 @@
+package dto
+
+type MedicationStatus string
+
+const (
+	MedicationOk      MedicationStatus = "ok"
+	MediactionExpired MedicationStatus = "expired"
+)
+
+type MedicationItemDTO struct {
+	Id     string           `json:"id"`
+	Name   string           `json:"name"`
+	Desc   string           `json:"desc"`
+	Price  int64            `json:"price"`
+	Expiry string           `json:"expiry"`
+	Status MedicationStatus `json:"status"`
+}
